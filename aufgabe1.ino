1d905e71f9a9193a07ffa27596ae36ad14d5ba82
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2018-10-08 17:33:24 +0200

    add initial ino file

--- aufgabe1.ino
+++ aufgabe1.ino
@@ -1,4 +1,29 @@
-void setup()
-{
-  pinMode(9, OUTPUT);
+// An dieser Stelle koennen Variablen deklariert und initialisiert werden.
+
+/* setup Funktion wird beim Programmstart einmalig ausgefuehrt */
+void setup() {
+    pinMode(9, OUTPUT);  // An Pin 9 ist eine rote LED angeschlossen
+    pinMode(8, OUTPUT);  // An Pin 8 ist eine orange LED angeschlossen
+    pinMode(7, OUTPUT);  // An Pin 7 ist eine gruene LED angeschlossen
 }
+
+/* loop Funktion wird wiederholt solange das Board mit Strom versorgt wird */
+void loop() {
+    digitalWrite(9, HIGH);  // Rote LED an
+    delay(5000);            // Warte 5 Sekunden
+
+    digitalWrite(8, HIGH);  // Orange LED an
+    delay(3000);            // Warte 3 Sekunden
+
+    digitalWrite(9, LOW);  // Rote LED aus
+    digitalWrite(8, LOW);  // Orange LED aus
+
+    digitalWrite(7, HIGH);  // Grune LED an
+    delay(5000);            // Warte 5 Sekunden
+
+    digitalWrite(8, HIGH);  // Orange LED an
+    delay(3000);            // Warte 3 Sekunden
+
+    digitalWrite(8, LOW);  // Orange LED aus
+    digitalWrite(7, LOW);  // Grune LED aus
+}  // und noch einmal
