3f84e017bc44360d15bf690a894b95e464c9c768
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2018-10-29 14:44:36 +0100

    add lesson 4

--- README.md
+++ README.md
@@ -1,19 +1,7 @@
 # Class: Sketching mit Hardware
 
-This repository contains the code for my university class "sketching with hardware", an introduction to working with hardware (Arduino) and C++.
+## Lesson 4: Async Control
 
-> Note: The comments within the code are written in German as of now.
+> This project is set up for compilation in Visual Studio Code using the [Arduino extension](https://github.com/Microsoft/vscode-arduino/). It should also compile in the default Arduino IDE, though some files might initially not be visible there.
 
-&nbsp;
-___
-### Lesson 1: Traffic Control
-Implementation of an automatic traffic control system, monitoring traffic with an ultrasonic sensor. Can also be controlled with an IR Remote.
-> see branch: [`traffic-control`](https://github.com/jonaskuske/sketching-with-hardware/tree/traffic-control)
-
-### Lesson 2: Blinking LED
-A simple LED, controllable with a button: it eithers blinks or turns off with a fade-out effect.
-> see branch: [`blinking-led`](https://github.com/jonaskuske/sketching-with-hardware/tree/blinking-led)
-
-### Lesson 3: Photo Resistor
-Using a photo resistor so an LED can respond to the ambient brightness.
-> see branch: [`photo-resistor`](https://github.com/jonaskuske/sketching-with-hardware/tree/photo-resistor)
+Using the `millis` function to let one LED blink while another responds to button presses at the same time.
--- sketching-with-hardware.ino
+++ sketching-with-hardware.ino
@@ -1,4 +1,58 @@
+#define BLINK_LED 3
+#define TRIGGER_LED 4
+#define BUTTON_PIN 5
+
 /**
- * Check out one of the other branches to see the code.
- * An overview can be found in README.md
+ * Einmaliges Setup, Pin-Mode konfigurieren.
  */
+void setup() {
+    pinMode(BLINK_LED, OUTPUT);
+    pinMode(TRIGGER_LED, OUTPUT);
+    pinMode(BUTTON_PIN, INPUT);
+}
+
+// Kontroll-Variablen für blinkende LED
+bool blinkLightIsOn = false;
+unsigned long lastBlinkLightChange = millis();
+
+// Kontroll-Variablen für Button-gesteuerte LED
+bool triggerLightIsOn = false;
+unsigned long lastTriggerLightChange = millis();
+
+/**
+ * Schaltet Button-gesteuerte LED ein und speichert Zeitpunkt.
+ */
+void turnLightOn() {
+    triggerLightIsOn = true;
+    digitalWrite(TRIGGER_LED, HIGH);
+    lastTriggerLightChange = millis();
+}
+
+/**
+ * Schaltet Button-gesteuerte LED aus.
+ */
+void turnLightOff() {
+    digitalWrite(TRIGGER_LED, LOW);
+    triggerLightIsOn = false;
+}
+
+/**
+ * Automatisch wiederholte Loop-Funktion.
+ * Eine LED alle 500ms blinken lassen und falls Button gedrückt,
+ * zweite LED einschalten, dann nach 1s wieder abschalten.
+ */
+void loop() {
+    unsigned long now = millis();
+
+    // Alle 500ms: Zustand der blinkenden LED umkehren
+    if (now - lastBlinkLightChange > 500) {
+        digitalWrite(BLINK_LED, blinkLightIsOn ? LOW : HIGH);
+        blinkLightIsOn = !blinkLightIsOn;
+        lastBlinkLightChange = now;
+    }
+
+    // Falls Button-gesteuertes Licht aus und Button gedrückt: anschalten
+    if (!triggerLightIsOn && digitalRead(BUTTON_PIN) == HIGH) turnLightOn();
+    // Falls Button-gesteuerte LED an und eingeschaltet vor min. 1s: abschalten
+    if (now - lastTriggerLightChange > 1000 && triggerLightIsOn) turnLightOff();
+}
