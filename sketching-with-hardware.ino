8827f1cae010541f7a30c7f920b007c82fef91a4
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2018-11-12 13:21:32 +0100

    add lesson 5

--- sketching-with-hardware.ino
+++ sketching-with-hardware.ino
@@ -1,58 +1,68 @@
-#define BLINK_LED 3
-#define TRIGGER_LED 4
-#define BUTTON_PIN 5
+#define RED_LED 3
+#define GREEN_LED 5
+#define BLUE_LED 6
+
+// Steuert, in wie großen Schritten die Farbkanäle heller/dunkel geschaltet werden
+int step = 1;
+// Geschwindigkeit, mit der die Farben angepasst werden
+int timeout = 5;
+
+// Speichert, welcher Übergang des Loops gerade aktiv ist (Rot->Grün, Grün->Blau, Blau->Rot)
+int transitionFrom = 0;
+
+// Helligkeitswert der einzelnen Farbkanäle speichern
+int redBrightness = 0;
+int greenBrightness = 0;
+int blueBrightness = 0;
 
 /**
- * Einmaliges Setup, Pin-Mode konfigurieren.
+ * Einmaliges Setup: Pin-Mode konfigurieren
+ * und Rot auf maximal stellen (Startfarbe des Color Loops)
  */
 void setup() {
-    pinMode(BLINK_LED, OUTPUT);
-    pinMode(TRIGGER_LED, OUTPUT);
-    pinMode(BUTTON_PIN, INPUT);
-}
+    pinMode(GREEN_LED, OUTPUT);
+    pinMode(RED_LED, OUTPUT);
+    pinMode(BLUE_LED, OUTPUT);
 
-// Kontroll-Variablen für blinkende LED
-bool blinkLightIsOn = false;
-unsigned long lastBlinkLightChange = millis();
-
-// Kontroll-Variablen für Button-gesteuerte LED
-bool triggerLightIsOn = false;
-unsigned long lastTriggerLightChange = millis();
-
-/**
- * Schaltet Button-gesteuerte LED ein und speichert Zeitpunkt.
- */
-void turnLightOn() {
-    triggerLightIsOn = true;
-    digitalWrite(TRIGGER_LED, HIGH);
-    lastTriggerLightChange = millis();
+    redBrightness = 255;
 }
 
 /**
- * Schaltet Button-gesteuerte LED aus.
- */
-void turnLightOff() {
-    digitalWrite(TRIGGER_LED, LOW);
-    triggerLightIsOn = false;
-}
-
-/**
- * Automatisch wiederholte Loop-Funktion.
- * Eine LED alle 500ms blinken lassen und falls Button gedrückt,
- * zweite LED einschalten, dann nach 1s wieder abschalten.
+ * Loop: Helligkeit der einzelnen Farbkanäle verringern oder erhöhen,
+ * sodass ein flüssiger Farbwechsel entsteht
  */
 void loop() {
-    unsigned long now = millis();
-
-    // Alle 500ms: Zustand der blinkenden LED umkehren
-    if (now - lastBlinkLightChange > 500) {
-        digitalWrite(BLINK_LED, blinkLightIsOn ? LOW : HIGH);
-        blinkLightIsOn = !blinkLightIsOn;
-        lastBlinkLightChange = now;
+    // Wenn Rot volle Helligkeit hat:
+    // gleichzeitig Helligkeit von Rot verringern und Helligkeit von Grün erhöhen.
+    // Rot->Grün als aktiven Übergang markieren, damit dieser Vorgang solange weiter läuft,
+    // bis der rote Kanal ausgeschaltet ist und der grüne Kanal volle Helligkeit erreicht hat
+    if (redBrightness == 255 || transitionFrom == RED_LED) {
+        transitionFrom = RED_LED;
+        redBrightness -= step;
+        greenBrightness += step;
     }
 
-    // Falls Button-gesteuertes Licht aus und Button gedrückt: anschalten
-    if (!triggerLightIsOn && digitalRead(BUTTON_PIN) == HIGH) turnLightOn();
-    // Falls Button-gesteuerte LED an und eingeschaltet vor min. 1s: abschalten
-    if (now - lastTriggerLightChange > 5000 && triggerLightIsOn) turnLightOff();
+    // Grün hat volle Helligkeit erreicht: Grün als aktiven Übergang vermerken
+    // -> ab jetzt Grün verringern & Blau erhöhen, bis Blau volle Helligkeit erreicht hat
+    if (greenBrightness == 255 || transitionFrom == GREEN_LED) {
+        transitionFrom = GREEN_LED;
+        greenBrightness -= step;
+        blueBrightness += step;
+    }
+
+    // Blau hat volle Helligkeit erreicht: Blau als aktiven Übergang vermerken
+    // -> ab jetzt Blau verringern & Rot erhöhen, bis Rot volle Helligkeit erreicht hat
+    if (blueBrightness == 255 || transitionFrom == BLUE_LED) {
+        transitionFrom = BLUE_LED;
+        blueBrightness -= step;
+        redBrightness += step;
+    }
+
+    // Die geänderten Werte am Ende jedes Durchlaufs des Loops auf die Farbkanäle anwenden
+    analogWrite(RED_LED, redBrightness);
+    analogWrite(GREEN_LED, greenBrightness);
+    analogWrite(BLUE_LED, blueBrightness);
+
+    // Delay, damit der Loop nicht zu schnell abläuft
+    delay(timeout);
 }
