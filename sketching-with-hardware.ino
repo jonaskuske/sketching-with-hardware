eb17de412ac4f9a14004ae7ddd6f61dc11f32426
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2019-10-15 15:24:56 +0200

    add lesson

--- sketching-with-hardware.ino
+++ sketching-with-hardware.ino
@@ -1,4 +1,21 @@
-/**
- * Check out one of the other branches to see the code.
- * An overview can be found in README.md
- */
+int lights[] = {7, 8, 9, 10, 11};
+
+int numLights = sizeof(lights) / sizeof(lights[0]);
+int currentLight = lights[0];
+
+void setup() {
+  // Set all connected pins to Output
+  for (int i = 0; i < numLights; i++) pinMode(lights[i], OUTPUT);
+}
+
+void loop() {
+  for (int i = 0; i < numLights; i++) {
+    // Turn off prev light
+    digitalWrite(currentLight, LOW);
+    // Set next light and turn on
+    currentLight = lights[i];
+    digitalWrite(currentLight, HIGH);
+
+    delay(100);
+  }
+}
